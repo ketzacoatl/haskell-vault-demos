72c244d6e6676bf5ce187087e7cb1b6c9c8dda8b
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2019-07-05 09:43:20 -0400

    Interact with vault with vaultHealth from vault-tool

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Lib
   ( someFunc
   ) where
 
+import Data.Text (pack, Text(..))
+import Network.VaultTool (vaultHealth, VaultHealth (..), VaultAddress (..))
+import Say (say)
+
+vaultURL :: VaultAddress
+vaultURL = VaultAddress "http://127.0.0.1:8200"
+
 someFunc :: IO ()
 someFunc = do
-  putStrLn "someFunc"
+  say "check vault status.."
+  (status :: VaultHealth) <- vaultHealth vaultURL
+  say $ pack $ show status
